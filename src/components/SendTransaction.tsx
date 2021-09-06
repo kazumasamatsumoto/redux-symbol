54e36e04416550c7e481a72042bbf714c4e0f158
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-09-06 22:03:09 +0900

    transaction send

--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import {
+  Account,
+  Address,
+  Deadline,
+  PlainMessage,
+  RepositoryFactoryHttp,
+  TransferTransaction,
+  UInt64,
+} from 'symbol-sdk'
 
 export const SendTransaction = () => {
+  const example = async (): Promise<void> => {
+    // Network information
+    const nodeUrl = 'http://ngl-dual-101.testnet.symboldev.network:3000'
+    const repositoryFactory = new RepositoryFactoryHttp(nodeUrl)
+    const epochAdjustment = await repositoryFactory
+      .getEpochAdjustment()
+      .toPromise()
+    const networkType = await repositoryFactory.getNetworkType().toPromise()
+    const networkGenerationHash = await repositoryFactory
+      .getGenerationHash()
+      .toPromise()
+    // Returns the network main currency, symbol.xym
+    const { currency } = await repositoryFactory.getCurrencies().toPromise()
+
+    /* start block 01 */
+    // replace with recipient address
+    const rawAddress = 'TBUKFL3BMEXYBDQYBV5Y7UOWNRM3TDRZ4PNFCZQ'
+    const recipientAddress = Address.createFromRawAddress(rawAddress)
+
+    const transferTransaction = TransferTransaction.create(
+      Deadline.create(epochAdjustment),
+      recipientAddress,
+      [currency.createRelative(10)],
+      PlainMessage.create('This is a test message'),
+      networkType,
+      UInt64.fromUint(2000000)
+    )
+    /* end block 01 */
+
+    /* start block 02 */
+    // replace with sender private key
+    const privateKey =
+      '72D29CA347E87A7C4205D90BE51A800931D87402DF34A1FB5BD533BBC623E1A9'
+    const account = Account.createFromPrivateKey(privateKey, networkType)
+    const signedTransaction = account.sign(
+      transferTransaction,
+      networkGenerationHash
+    )
+    console.log('Payload:', signedTransaction.payload)
+    console.log('Transaction Hash:', signedTransaction.hash)
+    /* end block 02 */
+
+    /* start block 03 */
+    const transactionRepository =
+      repositoryFactory.createTransactionRepository()
+    const response = await transactionRepository
+      .announce(signedTransaction)
+      .toPromise()
+    console.log(response)
+    /* end block 03 */
+  }
   return (
-    <div>
+    <button
+      className="text-white bg-indigo-600 hover:bg-indigo-700 rounded py-2 px-3 text-sm"
+      onClick={() => example().then()}
+    >
       トランザクション送信
-    </div>
+    </button>
   )
 }
